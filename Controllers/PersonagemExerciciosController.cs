2308d0a5bfee937c36a6e3412ae82bfc60a0d2b3
Edu <eduardokaue277@gmail.com>
2024-03-20 11:55:05 -0300

    Feat: New Controllers

--- Controllers/PersonagemExerciciosController.cs
+++ Controllers/PersonagemExerciciosController.cs
@@ -13,18 +13,18 @@ namespace RpgAPI.Controllers
 {
     [ApiController]
     [Route("[Controller]")]
-    public class PersonagemExerciciosController : Controller
+    public class PersonagemExerciciosController : ControllerBase
     {
         private static List<Personagem> personagens = new List<Personagem>()
         {
             //Colar os objetos da lista do chat aqui
             new Personagem() { Id = 1, Nome = "Frodo", PontosVida=100, Forca=17, Defesa=23, Inteligencia=33, Classe=ClasseEnum.Cavaleiro},
-            new Personagem() { Id = 2, Nome = "Sam", PontosVida=100, Forca=15, Defesa=25, Inteligencia=30, Classe=ClasseEnum.Cavaleiro},
-            new Personagem() { Id = 3, Nome = "Galadriel", PontosVida=100, Forca=18, Defesa=21, Inteligencia=35, Classe=ClasseEnum.Clerigo },
+            new Personagem() { Id = 2, Nome = "Sam", PontosVida=122, Forca=15, Defesa=25, Inteligencia=30, Classe=ClasseEnum.Cavaleiro},
+            new Personagem() { Id = 3, Nome = "Galadriel", PontosVida=170, Forca=18, Defesa=21, Inteligencia=35, Classe=ClasseEnum.Clerigo },
             new Personagem() { Id = 4, Nome = "Gandalf", PontosVida=100, Forca=18, Defesa=18, Inteligencia=37, Classe=ClasseEnum.Mago },
-            new Personagem() { Id = 5, Nome = "Hobbit", PontosVida=100, Forca=20, Defesa=17, Inteligencia=31, Classe=ClasseEnum.Cavaleiro },
+            new Personagem() { Id = 5, Nome = "Hobbit", PontosVida=150, Forca=20, Defesa=17, Inteligencia=31, Classe=ClasseEnum.Cavaleiro },
             new Personagem() { Id = 6, Nome = "Celeborn", PontosVida=100, Forca=21, Defesa=13, Inteligencia=34, Classe=ClasseEnum.Clerigo },
-            new Personagem() { Id = 7, Nome = "Radagast", PontosVida=100, Forca=25, Defesa=11, Inteligencia=35, Classe=ClasseEnum.Mago }
+            new Personagem() { Id = 7, Nome = "Radagast", PontosVida=110, Forca=25, Defesa=11, Inteligencia=35, Classe=ClasseEnum.Mago }
         };
 
         [HttpGet("GetByName/{nome}")]
@@ -54,30 +54,41 @@ namespace RpgAPI.Controllers
         public IActionResult GetClerigo()
         {
             personagens.RemoveAll(p => p.Classe == ClasseEnum.Cavaleiro);
-            return Ok("Personagems Removidos: " + personagens);
+            return Ok(personagens.OrderBy(pv => pv.PontosVida));
 
         }
 
         [HttpPost("PostValidMago")]
         public IActionResult PostValidMago (Personagem newPersonagem)
         {
-            if(newPersonagem.Classe = 2 && newPersonagem.Inteligencia < 35)
+            if(newPersonagem.Classe == ClasseEnum.Mago && newPersonagem.Inteligencia <35)
             {
-                return BadRequest("Não pode ser adicionado");
+               return BadRequest("Não é Adicionr o personagem");
             }
             
             personagens.Add(newPersonagem);
             return Ok(personagens);
 
         }
+        
 
         [HttpGet("GetEstatistica")]
 
         public IActionResult GetEstic()
         {
-            return Ok( "Quant Personagens: " + personagens.Count);
+            return Ok( "Quant Personagens: " + personagens.Count + "Soma de pontos de Inteligencia: " + personagens.Sum(p => p.Inteligencia));
             
         }
+
+        [HttpGet("GetByClasse/{id}")]
+        public IActionResult GetByName(int id)
+        {
+            List<Personagem> listaBusca = personagens.FindAll(p => p.Id == id);
+
+            return Ok(listaBusca);
+            
+        }
+
         
 
         
